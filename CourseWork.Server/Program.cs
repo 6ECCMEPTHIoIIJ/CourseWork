2a082b94f94e1e6854470339fd41e0db6dfed8cb
k1tt3nz <k1tt3nzpink@gmail.com>
2023-12-24 20:48:57 +0300

    add database connection

--- CourseWork.Server/Program.cs
+++ CourseWork.Server/Program.cs
@@ -1,8 +1,12 @@
+using CourseWork.Server.libs.utils;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Abstractions;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.Resource;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 
 namespace CourseWork.Server
 {
@@ -15,6 +19,22 @@ namespace CourseWork.Server
             // Add services to the container.
             builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+            try
+            {
+                var connectionString = "User ID=gen_user;Password=o*=1/.CX$J8ew/;Server=185.154.195.121;Port=5432;Database=default_db;Integrated Security=true;Pooling=true;";
+
+                builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseNpgsql(connectionString));
+
+                Console.WriteLine($"Подключения к базе данных успешно");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Ошибка подключения к базе данных: {ex.Message}");
+                Console.WriteLine(ex.ToString());
+            }
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ CourseWork.Server/libs/utils/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CourseWork.Server.libs.utils
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
